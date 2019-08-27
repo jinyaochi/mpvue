9a8956b11b388742ee06013cfde933d202ee06e3
duanzhiwei <89340545@qq.com>
2019-08-27 17:02:42 +0800

    df

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="box">
 
-    <button open-type="contact">进入客服会话</button>
-    <button open-type="getUserInfo" lang="zh_CN" bindgetuserinfo="onGotUserInfo">获取用户信息</button>
-    <button open-type="openSetting">打开授权设置页</button>
+    <map
+            id="map"
+            longitude="116.39987458203123"
+            latitude="39.90915359502086"
+            scale="14"
+            :markers="markers"
+            @markertap="markertap"
+            :polyline="polyline"
+            @begin="begin"
+            show-location
+    >
+
+      <cover-view class="controls">
+        <cover-view class="time">00:00</cover-view>
+      </cover-view>
+
+    </map>
 
   </div>
 </template>
@@ -56,10 +70,34 @@ export default {
 }
 </script>
 <style>
-  .button-hover {
-    background-color: red;
+  page,#box{
+    height: 100%;
   }
-  .other-button-hover {
-    background-color: blue;
+  #map{
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .controls {
+    position: relative;
+    top: 50%;
+    height: 50px;
+    margin-top: -25px;
+    display: flex;
+  }
+  .play,.pause,.time {
+    flex: 1;
+    height: 100%;
+  }
+  .time {
+    text-align: center;
+    background-color: rgba(0, 0, 0, .5);
+    color: white;
+    line-height: 50px;
+  }
+  .img {
+    width: 40px;
+    height: 40px;
+    margin: 5px auto;
   }
 </style>
