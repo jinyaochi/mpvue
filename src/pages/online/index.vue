e6b8b6790ab4c9cf1fb660179ba425c31f8a46aa
duan <1250101915@qq.com>
2019-09-18 11:37:13 +0800

    t

--- src/pages/online/index.vue
+++ src/pages/online/index.vue
@@ -2,8 +2,25 @@
   <div>
     <div class="online-head"><img src='/static/images/online-head.jpg' alt="" width="100%" mode="widthFix"></div>
     <div class="online-time">
-      <div class="one"></div>
-      <div class="one-body"></div>
+      <div class="font">
+        <div>
+          <p>
+            <span>第一期</span>
+            <span>表音密码</span>
+            <span>共10节</span>
+          </p>
+          <div class="felling">
+            <a href=""><span>1.2w</span><span class="icon icon-white love"></span></a>
+            <a href=""><span>1.4 </span><span class="icon icon-white good"></span></a>
+            <a href=""><span>100</span><span class="icon icon-white look"></span></a>
+          </div>
+        </div>
+
+      </div>
+      <div class="box">
+        <div class="one"></div>
+        <div class="one-body"></div>
+      </div>
     </div>
   </div>
 
@@ -11,6 +28,7 @@
 
 <script>
   import '@/assets/css/online.css';
+  import '@/assets/css/index.css';
 
 
 
