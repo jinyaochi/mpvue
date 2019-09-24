e88ba1a4d80a24f938bee88725d07ad88e32c66e
duan <1250101915@qq.com>
2019-09-24 21:25:22 +0800

    aa

---
+++ src/components/alert_choose.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="alert_back">
+      <div class="alert_box">
+        <div>
+          <p>确定删除收藏视频吗</p>
+          <div class="choose">
+            <div>取消</div>
+            <div>确定</div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import '@/assets/css/alert_font.css';
+
+
+  export default {
+    props:['type'],
+    name: "footer",
+    data:{
+
+    },
+    methods: {
+      redirect(url){
+        this.$location.rediect(url)
+      }
+    }
+  };
+</script>
