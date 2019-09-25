fe08d9faf23f49133a3a627a4bba51db010b4e11
duanzhiwei <89340545@qq.com>
2019-09-25 15:07:04 +0800

    df

--- src/pages/school_synopsis/index.vue
+++ src/pages/school_synopsis/index.vue
@@ -10,7 +10,7 @@
       </swiper>
       <div class="details-box2">
         <div class="details-title title">
-          <p>能动英语清河校区</p>
+          <p>{{school.name}}</p>
           <span @click="backup">关闭</span>
         </div>
 
@@ -18,7 +18,7 @@
           <div class="title">
             <p class="details-top">校区简介</p>
           </div>
-          <div class="details-synopsis bb-1">今年1月，央行宣布全面降准1个百分点；5月，央行决定对聚焦当地、服务县域的中小银行，实行较低的优惠存款准备金率。 兴业银行首席经济学家鲁政委分析，此次降准力度有3点超预期之处</div>
+          <div class="details-synopsis bb-1">{{school.intro}}</div>
         </div>
 
 
