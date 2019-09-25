11c7b3fde2185d1ec8f4d8a0464af06a7cb9b878
duan <1250101915@qq.com>
2019-09-25 14:42:03 +0800

    111

--- src/pages/video_details/index.vue
+++ src/pages/video_details/index.vue
@@ -1,10 +1,104 @@
 <template>
   <div class="">
     <div class="video_top"></div>
-    <div></div>
-    <div>
-      <p></p>
-      <span></span>
+    <div class="video_box"></div>
+    <div class="inContainer video_blue">
+      <p>单词记不住？会说不会写？单词记不住？会说不会写？单词记不住？会说不会写？</p>
+      <span>简介</span>
+    </div>
+    <div class="inContainer">
+      <div class="clearfix">
+        <div class="font">
+          <div>
+            <a href=""><span>qqqq</span><span class="icon love"></span></a>
+            <a href=""><span>1111</span><span class="icon good"></span></a>
+            <a href=""><span>2222</span><span class="icon look"></span></a>
+          </div>
+        </div>
+        <div class="head-img">
+          <img mode="widthFix" alt="">
+        </div>
+      </div>
+      <div class="video_head">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+        <img src="http://admin.qq.im/static/images/head-img1.png" alt="">
+
+      </div>
+
+
+      <ul class="details-comment">
+        <li>
+          <img src="http://admin.qq.im/static/images/head-img1.png" mode="widthFix" alt="">
+          <div class="comment-box">
+            <div class="comment-title">
+              <h2>我是郭德纲</h2>
+              <span>2019-12-31</span>
+            </div>
+            <p class="comment-content">
+              校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了
+            </p>
+            <div class="comment-reply">
+              <span>热血</span>
+              <span class="black">&nbsp;回复&nbsp;</span>
+              <span>冲鸭:</span>
+              <span>我觉得还行吧，一般</span>
+            </div>
+            <div class="comment-reply">
+              <span>热血</span>
+              <span class="black">&nbsp;回复&nbsp;</span>
+              <span>冲鸭:</span>
+              <span>我觉得还行吧，一般</span>
+            </div>
+
+            <div class="comment-choose">
+              <a href="">
+                <span class="comment-img"><img class="details-img2" src="http://admin.qq.im/static/images/details-zan.png" alt="" mode="aspectFill">&nbsp;&nbsp;</span>
+                <span>0</span>
+              </a>
+              <a href="">
+                <span class="comment-img"><img class="details-img2" src="http://admin.qq.im/static/images/details-more.png" alt="" mode="aspectFill">&nbsp;&nbsp;</span>
+                <span>回复</span>
+              </a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <img src="http://admin.qq.im/static/images/head-img1.png" mode="widthFix" alt="">
+          <div class="comment-box">
+            <div class="comment-title">
+              <h2>我是郭德纲</h2>
+              <span>2019-12-31</span>
+            </div>
+            <p class="comment-content">
+              校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了校区环境不错，老师也用专业，选对了
+            </p>
+
+            <div class="comment-choose">
+              <a href="">
+                <span class="comment-img"><img class="details-img2" src="http://admin.qq.im/static/images/details-zan.png" alt="" mode="aspectFill">&nbsp;&nbsp;</span>
+                <span>0</span>
+              </a>
+              <a href="">
+                <span class="comment-img"><img class="details-img2" src="http://admin.qq.im/static/images/details-more.png" alt="" mode="aspectFill">&nbsp;&nbsp;</span>
+                <span>回复</span>
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div style="height: 116px"></div>
+      <div class="comment-inp">
+        <input type="text" placeholder="我来说几句" placeholder-class="comment-inp-color">
+      </div>
     </div>
   </div>
 
